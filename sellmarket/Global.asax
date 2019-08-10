830a2face37895264c4565abc72a0790625d9f93
moham <moham@DESKTOP-AS136SM>
2019-08-10 20:12:03 +0430

    Add project files.

---
+++ sellmarket/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sellmarket.MvcApplication" Language="C#" %>
---
+++ sellmarket/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sellmarket.Startup))]
+namespace sellmarket
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
